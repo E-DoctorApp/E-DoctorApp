37076145047c16b96356493c48f63f71d02087e4
yacine-bencheikh <ybenchikh472@gmail.com>
2023-10-10 20:12:46 +0100

    fin day

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,21 +1,19 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import DoctorProfile from './components/DoctorProfile/DoctorProfile';
 import ContactUs from './components/contactUs/ContactUS';
 import NavBar from './components/NavBar/NavBar';
 import Footer from './components/Footer/Footer';
 import LandingPage from './components/LandingPage/LandingPage';
+import DoctorProfile from './components/DoctorProfile/DoctorProfile';
 
 function App() {
   return (
     <div className="App">
-      <DoctorProfile/>
-      {/* <ContactUs/> */}
       <NavBar />
       <ContactUs />
 
-
+      <DoctorProfile/>
 
       <LandingPage />
 
