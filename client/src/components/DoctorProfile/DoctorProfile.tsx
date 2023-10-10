bc956e75ec2fd3532bd5c4d7c812ac8ffd651a0c
leithayad <leithayadi188@gmail.com>
2023-10-10 17:23:43 +0200

    topnav and sidebar adjusted

--- client/src/components/DoctorProfile/DoctorProfile.tsx
+++ client/src/components/DoctorProfile/DoctorProfile.tsx
@@ -53,8 +53,8 @@ const DoctorProfile = () :  React.JSX.Element => {
               <div className="DoctorProfile-user">
               <div className="DoctorProfile-image-frame"><img src="https://www.perfocal.com/blog/content/images/size/w960/2021/01/Perfocal_17-11-2019_TYWFAQ_100_standard-3.jpg" alt="" /></div>
               <div className="DoctorProfile-details">
-                <p className="DoctorProfile-name">Stephen Conley</p>
-                <p>Cardiologist</p>
+                <span className="DoctorProfile-name">Stephen Conley</span>
+                <span>Cardiologist</span>
               </div>
               </div>
           </div>
