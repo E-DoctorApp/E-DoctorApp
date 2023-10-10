48d75c4daba9a482f3bfc80fc685204f806cc77f
yacine-bencheikh <ybenchikh472@gmail.com>
2023-10-10 13:25:27 +0100

    conntactUs

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,42 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-<<<<<<< HEAD
 import ContactUs from './components/contactUs/ContactUS';
-=======
 import NavBar from './components/NavBar/NavBar';
 import Footer from './components/Footer/Footer';
->>>>>>> 2eecf44c8ba664737f98a3daca40b3d6c752306b
 
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
       <ContactUs/>
-=======
-
-
-
-
-
-
-
-
-      <NavBar />
-
-
-
-
-
-
-
-      <Footer/>
-
-
-
-
-
->>>>>>> 2eecf44c8ba664737f98a3daca40b3d6c752306b
     </div>
   );
 }
