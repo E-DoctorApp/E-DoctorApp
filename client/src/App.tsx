37923f108549d9abb5ea618fbb346e2bf4cab8c7
Blhasn-Sehli <sehlibellhassen@gmail.com>
2023-10-10 20:26:26 +0200

    landingPageDone

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,12 +4,18 @@ import './App.css';
 import ContactUs from './components/contactUs/ContactUS';
 import NavBar from './components/NavBar/NavBar';
 import Footer from './components/Footer/Footer';
+import LandingPage from './components/LandingPage/LandingPage';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
       {/* <ContactUs /> */}
+
+
+
+      <LandingPage />
+
 
 
 
