6e8d70df58063c8f2a7a093a75f3db1bd57ed8e2
azizarf <aziz95arfaoui@gmail.com>
2023-10-11 19:03:06 +0100

    done color

--- client/src/components/Login/Form.tsx
+++ client/src/components/Login/Form.tsx
@@ -95,7 +95,7 @@ function Form() {
         <button
           type="button"
           className="btn btn-primary btn-lg"
-          style={{ paddingLeft: "2.5rem", paddingRight: "2.5rem" }}
+          style={{ paddingLeft: "2.5rem", paddingRight: "2.5rem" ,background:"#007E85",border:"none"}}
           onClick={(e) => {
             e.preventDefault();
             dispatch(
