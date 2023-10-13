e36319a65e2d142c384f09b3b8812a4671a5cae0
yacine-bencheikh <ybenchikh472@gmail.com>
2023-10-13 22:42:35 +0100

    nothing too

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 require('dotenv').config()
 const express = require('express')
+const faker = require('faker');
 const app = express()
 const db = require("./database/index")
 const PORT = process.env.PORT || 3000
@@ -16,7 +17,19 @@ app.use('/api/patient/',patientRouter)
 app.use("/api/review",reviewRouter)
 app.use("/api/room",roomRouter)
 
-
+app.get("/users", (req, res, next) => {
+    res.json([
+        {
+            "name": faker.name.findName(),
+            "email": faker.internet.email(),
+            "postCode": faker.address.zipCode(),
+            "city": faker.address.cityName(),
+            "country": faker.address.country(),
+            "phoneNumber": faker.phone.phoneNumber(),
+            "favouriteQuote": faker.lorem.sentence()
+        }
+    ]);
+});
 
 
 
