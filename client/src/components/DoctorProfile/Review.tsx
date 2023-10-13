cba8329172756ee0f4cf9d78d5389fa23bdc413c
yacine-bencheikh <ybenchikh472@gmail.com>
2023-10-13 21:50:26 +0100

    nothing

--- client/src/components/DoctorProfile/Review.tsx
+++ client/src/components/DoctorProfile/Review.tsx
@@ -3,7 +3,13 @@ import './review.css'
 import YasBar from "./YasBar";
 
 const Review = () => {
-    
+    const [job, setJob] = useState("")
+    const [profilePhoto] = useState("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKaiKiPcLJj7ufrj6M2KaPwyCT4lDSFA5oog&usqp=CAU")
+    const [name,setName] = useState("")
+    const [rate,setRate] = useState(0)
+    const [star,setStar] = useState(0)
+    const [trust, setTrust] = useState(25)
+
     return (
         <div style={{marginTop:"2rem"}}>
             <div className="title-place-yassine">
@@ -15,29 +21,29 @@ const Review = () => {
                         <div className="carousel-inner son-of-father ">
                             <div className="carousel-item active profile-card p-5">
                                 <img className="rounded-circle shadow-1-strong mb-4"
-                                    src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/img%20(10).webp" alt="avatar"
+                                    src={`${profilePhoto}`} alt="avatar"
                                     style={{ width: "150px" }} />
                                 <div className="row d-flex justify-content-center profile-section">
                                     <div className="col-lg-8 profile-section">
-                                        <h5 className="mb-3">Dr. Stephen Conley</h5>
-                                        <p>Cardiologist</p>
+                                        <h5 className="mb-3">{name}</h5>
+                                        <p>{job}</p>
                                         <div className="boutouna" ><i className="fa-solid fa-pencil"></i> boutouna</div><br />
                                         <p style={{ fontWeight: "bold", fontSize: "1rem" }}>146 Rates</p>
                                     </div>
                                 </div>
                                 <ul className="list-unstyled d-flex justify-content-center text-warning mb-3">
-                                    <li><i className="fas fa-star fa-sm"></i></li>
-                                    <li><i className="fas fa-star fa-sm"></i></li>
-                                    <li><i className="fas fa-star fa-sm"></i></li>
-                                    <li><i className="fas fa-star fa-sm"></i></li>
-                                    <li><i className="far fa-star fa-sm"></i></li>
+                                    <li><i className={ star >= 1?"fas fa-star fa-sm" : "far fa-star fa-sm" }></i></li>
+                                    <li><i className= { star >= 2? "fas fa-star fa-sm":"far fa-star fa-sm" }></i></li>
+                                    <li><i className= { star >= 3? "fas fa-star fa-sm":"far fa-star fa-sm" }></i></li>
+                                    <li><i className= { star >= 4? "fas fa-star fa-sm":"far fa-star fa-sm" }></i></li>
+                                    <li><i className= { star >= 5? "fas fa-star fa-sm":"far fa-star fa-sm" }></i></li>
                                 </ul>
                                 <div className="d-flex justify-content-between mb-2">
                                     <div>Trust</div>
-                                    <div>95%</div>
+                                    <div>{trust}%</div>
                                 </div>
                                 <div className="progress " style={{ width: "100%" }}>
-                                    <div className="progress-bar  bg-success" role="progressbar" style={{ width: "95%" }} aria-valuenow={100} aria-valuemin={0} aria-valuemax={100}></div>
+                                    <div className="progress-bar  bg-success" role="progressbar" style={{width:`${trust}%`}} aria-valuenow={100} aria-valuemin={0} aria-valuemax={100}></div>
                                 </div>
                             </div>
                         </div>
--- server/index.js
+++ server/index.js
@@ -5,12 +5,17 @@ const db = require("./database/index")
 const PORT = process.env.PORT || 3000
 const patientRouter = require("./routers/patient.router.js")
 const doctorRouter = require("./routers/doctor.router")
+const reviewRouter = require("./routers/review.router")
+const roomRouter = require("./routers/room.router")
 const cors = require("cors")
 
 app.use(express.json())
 app.use(cors())
 app.use("/api/doctor", doctorRouter);
 app.use('/api/patient/',patientRouter)
+app.use("/api/review",reviewRouter)
+app.use("/api/room",roomRouter)
+
 
 
 
