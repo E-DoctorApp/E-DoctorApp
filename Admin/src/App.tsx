74881118370e88dd660b0671e964d4645e98e165
Blhasn-Sehli <sehlibellhassen@gmail.com>
2023-10-15 21:06:56 +0200

    last version

--- Admin/src/App.tsx
+++ Admin/src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { BrowserRouter as Router, Route} from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Navbar from "./components/navbar/Navbar";
 import Reports from "./components/Reports/Reports";
 import ContactUs from "./components/ContactUs/ContuctUs";
@@ -8,9 +8,9 @@ import axios from "axios";
 function App() {
   const [id, setId] = useState("");
 
-  const handleUpdate = async (body:object) => {
+  const handleUpdate = async (body: object) => {
     try {
-      const response = await axios.put(`http://localhost:5000/api/AdminDoc/${id}`,body);
+      const response = await axios.put(`http://localhost:5000/api/AdminDoc/${id}`, body);
       console.log("Data updated successfully:", response.data);
       return response.data;
     } catch (error) {
@@ -24,27 +24,14 @@ function App() {
   // }, []);
 
   return (
-    <Router>
-      <div>
-        <div>
-          {/* <Navbar /> */}
-        </div>
-        <div>
-          {/* <Switch> */}
-            <Route path="/reports">
-              <Reports />
-            </Route>
-            <Route path="/contact">
-              {/* <ContactUs /> */}
-            </Route>
-            <Route path="/">
-              {/* Default route, you can render any component here */}
-              <h1>Welcome to the homepage</h1>
-            </Route>
-          {/* </Switch> */}
-        </div>
-      </div>
-    </Router>
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Reports/> } />
+        <Route path="/contact" element={<ContactUs/>} />
+      </Routes>
+    </div>
+
   );
 }
 
